7d29b2fc5c6c5fbe5bb777c35dd6bd2f301aea8d
Hardik Goel <hardik.iitr@gmail.com>
2023-03-16 12:27:47 -0700

    Create

---
+++ scripts/devbox-setup.ps1
@@ -0,0 +1,6 @@
+iwr -useb get.scoop.sh | iex
+scoop install curl sudo jq
+
+scoop install neovim gcc
+scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+scoop install fzf
