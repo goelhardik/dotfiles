862ef8d93dfcf4215574bba131a44ca756287d7c
Hardik Goel <hardik.iitr@gmail.com>
2023-03-16 12:28:00 -0700

    Create

---
+++ scripts/ps-modules.ps1
@@ -0,0 +1,5 @@
+Install-Module posh-git -Force -Scope AllUsers
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force -Scope AllUsers
+Install-Module -Name z -Force -Scope AllUsers
+Install-Module -Name PSReadLine -AllowPrerelease -Force -Scope AllUsers -SkipPublisherCheck
+Install-Module -Name PSFzf -Force -Scope AllUsers
