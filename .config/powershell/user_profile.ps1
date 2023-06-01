32468a61cdbaf03c5a1d81f80bf2bb1c8368a00a
Hardik Goel <hardik.iitr@gmail.com>
2023-06-01 15:53:44 -0700

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,6 +1,5 @@
 # Prompt
 Import-Module posh-git
-Import-Module oh-my-posh
 
 # Load prompt config
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
@@ -57,10 +56,53 @@ Set-Alias gco 'git checkout'
 
 ## Other
 Set-Alias c clear
+
+
+####### ALIASES
+
+# kubectl aliases
+Function kubectlCurrentContext {kubectl config current-context}
+
+Set-Alias -Name k -Value kubectl
+Set-Alias -Name kc -Value kubectlCurrentContext
+
+# git aliases
+Function gitstatus {git status $args}
+Function gitadd {git add $args}
+Function gitcheckout {git checkout $args}
+Function gitdiff {git diff $args}
+Function gitpull {git pull $args}
+Function gitbranch {git branch $args}
+Set-Alias -Name gs -Value gitstatus
+Set-Alias -Name ga -Value gitadd
+Set-Alias -Name gco -Value gitcheckout
+Set-Alias -Name gd -Value gitdiff
+Set-Alias -Name gpu -Value gitpull
+Set-Alias -Name gb -Value gitbranch
+
+# ado aliases
+Function startskyrise {cd D:\git\ado\src; ./init.ps1 -KeepPsReadLine $True}
+Set-Alias -Name ado -Value startskyrise
+
+# gitops aliases
+Function gitopsdir {cd D:\git\gitops\gitops}
+Set-Alias -Name gitops -Value gitopsdir
+
+Set-Alias -Name rider -Value 'C:\Program Files\JetBrains\JetBrains Rider 2021.3.3\bin\rider64'
+
 ##########################################
 
 # Utilities
 function which ($command) {
 	Get-Command -Name $command -ErrorAction SilentlyContinue |
 	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+
+# Write the current path as the terminal title
+$gitDir = git rev-parse --show-toplevel 2>$null
+if ($null -ne $gitDir) {
+	$Host.UI.RawUI.WindowTitle = $gitDir
+} else {
+	$Host.UI.RawUI.WindowTitle = $currentPath
 }
